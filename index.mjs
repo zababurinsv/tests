539d964b43eabcdd6d3cafcc3911b199e7217817
Sergey <s.zababurin.v@gmail.com>
2021-01-02 21:00:57 +0300

    update

--- index.mjs
+++ index.mjs
@@ -1,19 +1,28 @@
 import emoji from '/static/html/components/component_modules/emoji/emoji.mjs';
-import events from '/static/html/components/component_modules/CustomEvent/index.mjs'
+import task from '/static/html/components/component_modules/heap/index.mjs'
 
 describe('task manager', async function () {
   this.timeout(10000);
   before(async function () {
+    console.log('task:', task)
     console.log('emoji', emoji('all'))
   });
   it('# set task before', function () {
     return new Promise(async (resolve, reject) => {
-      resolve(false)
+      task.get(true, 'await', '5', '','/test-heap', async (object)=>{
+        object.callback({status:true})
+      })
+      resolve()
     })
   })
   it('# set task after', function () {
     return new Promise(async (resolve, reject) => {
-      resolve(false)
+    let res = await task.set(true, 'get test', '5', {
+        test:"ok"
+      },'/test-heap')
+      (res.status)
+      ?resolve()
+      :reject()
     })
   })
 })
